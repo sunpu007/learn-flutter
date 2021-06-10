6e7dfb8c39185f8b2c3b926284759e8af2af2825
Jerry_sun <731238433@qq.com>
2021-06-10 16:56:02 +0800

    'Tween'

--- lib/views/group/group.dart
+++ lib/views/group/group.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class GroupPage extends StatefulWidget {
@@ -9,14 +11,18 @@ class _GroupPageState extends State<GroupPage> with SingleTickerProviderStateMix
 
   AnimationController _controller;
 
+  Animation<double> _sizeAnim;
+  Animation<double> _rotationAnim;
+
+
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(vsync: this, lowerBound: 50, upperBound: 150, duration: Duration(milliseconds: 1000));
-    _controller.addListener(() {
-      setState(() {
-      });
-    });
+    _controller = AnimationController(vsync: this, duration: Duration(milliseconds: 1000));
+
+    _sizeAnim = Tween(begin: 50.0, end: 150.0).animate(_controller);
+    _rotationAnim = Tween(begin: 0.0, end: pi * 2).animate(_controller);
+
     _controller.addStatusListener((AnimationStatus status) {
       if (status == AnimationStatus.completed) {
         _controller.reverse();
@@ -32,19 +38,30 @@ class _GroupPageState extends State<GroupPage> with SingleTickerProviderStateMix
       appBar: AppBar(
         title: Text('小组'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(Icons.favorite, color: Colors.red, size: _controller.value,),
-            RaisedButton(
-              child: Icon(Icons.play_arrow),
-              onPressed: () {
-                _controller.forward();
-              },
-            )
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          AnimatedBuilder(
+            animation: _controller,
+            builder: (ctx, child) {
+              return Transform(
+                alignment: Alignment.center,
+                transform: Matrix4.rotationZ(_rotationAnim.value),
+                child: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                  size: _sizeAnim.value,
+                )
+              );
+            },
+          ),
+          RaisedButton(
+            child: Icon(Icons.play_arrow),
+            onPressed: () {
+              _controller.forward();
+            },
+          ),
+        ],
       ),
     );
   }
